3360afb3f60e6cbbde0eee3c70578f2360de9dd1
Randdose <randomnes037@gmail.com>
2023-05-10 18:13:37 +0400

    commented out everything until I find solution to fetch problems

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
-fetch('../settings/settings.json', {
-	method: "POST",
-	headers: {
-		"Content-Type": "text/plain"
-})
-.then((response) => response.json())
-.then((json) => {
-
-	const themeName = json.appearance[0].theme;
-	console.log(themeName);
-
-});
+// fetch('../settings/settings.json', {
+// 	method: "POST",
+// 	headers: {
+// 		"Content-Type": "text/plain"
+// })
+// .then((response) => response.json())
+// .then((json) => {
+//
+// 	const themeName = json.appearance[0].theme;
+// 	console.log(themeName);
+//
+// });
 
 /*fetch(`../assets/themes/${themeName}.json`)
 .then((response) => response.json())
